f499b0822edb9f1a39afe22ac3d902dc5e764b97
alumno <alumno@ubuntu910.(none)>
2011-03-15 20:32:15 +0100

    parte primera terminada

--- practica1/ejemplo1.c
+++ practica1/ejemplo1.c
@@ -27,6 +27,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 #include <stdio.h>
+#include <math.h>
 
 #include <libplayerc/playerc.h>
 
@@ -35,6 +36,8 @@ main(int argc, const char **argv)
 {
   int i;
   playerc_client_t *client;
+  player_pose2d_t position2d_vel;
+  player_pose2d_t position2d_target;
   playerc_position2d_t *position2d;
 
   // Create a client and connect it to the server.
@@ -47,19 +50,70 @@ main(int argc, const char **argv)
   if (playerc_position2d_subscribe(position2d, PLAYER_OPEN_MODE))
     return -1;
 
-  // Make the robot spin!
-  if (0 != playerc_position2d_set_cmd_vel(position2d, 0.25, 0, DTOR(40.0), 1))
+// Make the robot spin!
+ if (0 != playerc_position2d_set_cmd_vel(position2d, 0.25, 0, DTOR(40.0), 1))
     return -1;
 
-  for (i = 0; i < 200; i++)
+  for (i = 0; i < 50; i++)
   {
-    // Wait for new data from server
-    playerc_client_read(client);
+		playerc_client_read(client);
 
-    // Print current robot pose
-    printf("position2d : %f %f %f\n",
-           position2d->px, position2d->py, position2d->pa);
+		// Print current robot pose
+		printf("position2d : %f %f %f\n",
+		position2d->px, position2d->py, position2d->pa);
   }
+/* con el comando playerc_position2d_set_cmd_vel() simplemente se establece una
+ * consigna de velocidad la cual es independiente de la posición en la que se
+ * encuentra el robot */
+
+
+
+
+      position2d_target.px = 2;
+      position2d_target.py = -3;
+      position2d_target.pa = 0;
+      
+      // Move to pose  
+      playerc_position2d_set_cmd_pose(position2d, position2d_target.px , position2d_target.py, position2d_target.pa , 1);
+
+	printf("position2d : %f %f %f\n",
+	position2d->px, position2d->py, position2d->pa);
+      // Stop when reach the target
+      while  (sqrt(pow(position2d->px - position2d_target.px,2.0) + pow(position2d->py - position2d_target.py,2.0)) > 0.05 )
+	{
+		playerc_client_read(client);
+
+		// Print current robot pose
+		printf("position2d : %f %f %f\n",
+		position2d->px, position2d->py, position2d->pa);
+	}
+/* Con el comando position2d_set_cmd_pose() se establece una pose de destino.
+ * Cuando se utiliza este método la información obtenida de la odometría sí que
+ * afecta a la trayectoria seguida y al punto final */
+
+
+      position2d_target.px = 0;
+      position2d_target.py = -3;
+      position2d_target.pa = 0;
+      position2d_vel.px = 0.6;
+      position2d_vel.py = 0;
+      position2d_vel.pa = 0;
+
+      // Move to pose  
+      playerc_position2d_set_cmd_pose_with_vel(position2d, position2d_target, position2d_vel, 1);
+
+      // Stop when reach the target
+      while  (sqrt(pow(position2d->px - position2d_target.px,2.0) + pow(position2d->py - position2d_target.py,2.0)) > 0.05 )
+	{
+		playerc_client_read(client);
+
+		// Print current robot pose
+		printf("position2d : %f %f %f\n",
+		position2d->px, position2d->py, position2d->pa);
+	}
+/* Con el comando playerc_position2d_set_cmd_pose_with_vel se hace lo mismo que
+ * con playerc_position2d_set_cmd_pose() pero además se puede indicar una
+ * consigna de velocidad */
 
   // Shutdown
   playerc_position2d_unsubscribe(position2d);
