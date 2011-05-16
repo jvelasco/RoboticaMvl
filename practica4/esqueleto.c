3c4e066d13fb51b7c2b2936e4565d21bb8b7c926
Jose Velasco <jossmail@gmail.com>
2011-05-16 22:44:24 +0200

    Inicaliacion de proxys

--- practica4/esqueleto.c
+++ practica4/esqueleto.c
@@ -8,8 +8,9 @@ main(int argc, const char **argv)
 {
   int i;  
   playerc_client_t *client;
-  playerc_position2d_t *position2d;
+  playerc_position2d_t *position2d_amcl,*position2d_vfh;
   player_pose2d_t position2d_target;
+  
   double arrayx[5]={ 5.0 ,  -5.0 , -5.0 ,  5.0 ,  5.0};
   double arrayy[5]={ 2.5 ,   2.5 , -2.5 , -2.5 ,  0.0};
   double arraya[5]={3.14 , -1.57 ,  0.0 , 1.57 , 1.57};
@@ -24,7 +25,8 @@ main(int argc, const char **argv)
   player_color_t color;
   puntos=(player_point_2d_t *)malloc(sizeof(player_point_2d_t)*(1)); //(1) punto
   color.red=255; color.green=0; color.blue=0;
-  
+  //localize:
+  playerc_localize_t *localize;
 
   // Create a client and connect it to the server.
   client = playerc_client_create(NULL, "localhost", 6665);
@@ -36,15 +38,22 @@ main(int argc, const char **argv)
 
 
   // Create and subscribe to a position2d device.
-  position2d = playerc_position2d_create(client, 0);
-  if (playerc_position2d_subscribe(position2d, PLAYER_OPEN_MODE) != 0)
+  position2d_vfh = playerc_position2d_create(client, 1);
+  if (playerc_position2d_subscribe(position2d_vfh, PLAYER_OPEN_MODE) != 0)
+    {
+      fprintf(stderr, "error: %s\n", playerc_error_str());
+      return -1;
+    }
+
+  position2d_amcl = playerc_position2d_create(client, 2);
+  if (playerc_position2d_subscribe(position2d_amcl, PLAYER_OPEN_MODE) != 0)
     {
       fprintf(stderr, "error: %s\n", playerc_error_str());
       return -1;
     }
 
   // Fixing initial position
-  playerc_position2d_set_odom(position2d,5.0,0.0,1.57);
+  //playerc_position2d_set_odom(position2d,5.0,0.0,1.57);
 
   // Create and subscribe to a sonar device
   
@@ -56,6 +65,17 @@ main(int argc, const char **argv)
       return -1;
     }
 
+  //crear y suscribir localize
+  localize = playerc_localize_create(client,0);
+  if (playerc_localize_subscribe(localize, PLAYER_OPEN_MODE) != 0)
+    {
+      fprintf(stderr, "error: %s\n", playerc_error_str());
+      return -1;
+    }
+
+  //crear set de particulas
+  playerc_localize_get_particles(localize);
+
   // Fix colour
   playerc_graphics2d_setcolor (graficos, color); 
 
@@ -63,7 +83,7 @@ main(int argc, const char **argv)
   playerc_graphics2d_clear(graficos); 
 
   // Enable motors 
-  playerc_position2d_enable(position2d,1);
+  playerc_position2d_enable(position2d_vfh,1);
   
   for ( i=0 ; i<5 ; i++ )
     {
@@ -72,22 +92,22 @@ main(int argc, const char **argv)
       position2d_target.pa = arraya[i];
       
       // Move to pose  
-      playerc_position2d_set_cmd_pose(position2d, position2d_target.px , position2d_target.py, position2d_target.pa , 1);
+      playerc_position2d_set_cmd_pose(position2d_vfh, position2d_target.px , position2d_target.py, position2d_target.pa , 1);
 
       // Stop when reach the target
-      while  (sqrt(pow(position2d->px - position2d_target.px,2.0) + pow(position2d->py - position2d_target.py,2.0)) > 0.05 )
+      while  (sqrt(pow(position2d_vfh->px - position2d_target.px,2.0) + pow(position2d_vfh->py - position2d_target.py,2.0)) > 0.05 )
 	{
 	  // Wait for new data from server
 	  playerc_client_read(client);
 	  
 	  // Print current robot pose
-	  printf("position2d : x %f y %f th %f stall %d\n",position2d->px, position2d->py, position2d->pa, position2d->stall); 
+	  printf("position2d : x %f y %f th %f stall %d\n",position2d_amcl->px, position2d_amcl->py, position2d_amcl->pa, position2d_amcl->stall); 
 	  // What does mean stall?
 	  // x, y, th, world frame or robot frame?
 
 	  // Draw current robot pose
-	  puntos[0].px=position2d->px;
-	  puntos[0].py=position2d->py;
+	  puntos[0].px=position2d_amcl->px;
+	  puntos[0].py=position2d_amcl->py;
 	  playerc_graphics2d_draw_points (graficos, puntos, 1);
 	  
 	  // Print sonar readings
@@ -98,7 +118,9 @@ main(int argc, const char **argv)
 
   // Unsuscribe and Destroy
   // position2d
-  playerc_position2d_unsubscribe(position2d); playerc_position2d_destroy(position2d);
+  playerc_position2d_unsubscribe(position2d_vfh); playerc_position2d_destroy(position2d_vfh);
+  playerc_position2d_unsubscribe(position2d_amcl); playerc_position2d_destroy(position2d_amcl);
+
   // sonar
 
   // graphics2d
